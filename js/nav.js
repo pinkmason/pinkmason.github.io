b1f968fb3057832a9972afaa49fd0971f50d8c3d
pinkmason <3189132051@qq.com>
2026-03-15 09:48:39 +0800

    调整

--- js/nav.js
+++ js/nav.js
@@ -16,45 +16,6 @@ function handleKeyPress(event) {
 }
 
 
-//随机排列网站
-/* // 数组随机化函数
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
-//随机父元素下子元素
-function randomizeElements(activewebsite) {
-    //const activewebsite = document.querySelector('.website.active');//获取div对象
-    const websiteitems = Array.from(activewebsite.children);
-    const shuffledItems = shuffleArray(websiteitems);
-    // 清除div子项
-    while (activewebsite.firstChild) {
-        activewebsite.removeChild(activewebsite.firstChild);
-    }
-    // 添加子项
-    shuffledItems.forEach(item => activewebsite.appendChild(item));
-} */
-
-
-//导航栏点击切换脚本，需css中的.website类display：none属性和.website.active类配合实现
-/* document.querySelectorAll(".nav-item.linksite").forEach(function (ni) {
-    ni.addEventListener('click', function () {
-        //console.log(this);
-        var webs = document.querySelectorAll('.website');
-        // 遍历所有的内容容器，并隐藏它们
-        webs.forEach(function (web) {
-            web.classList.remove('active'); // 移除active类，隐藏内容
-        });
-        var nowsite = document.getElementById(this.getAttribute('data-linksite'));
-        nowsite.classList.add('active'); // 添加active类，显示内容
-        //randomizeElements(nowsite); //随机排列网站
-    })
-}) */
-
-
 //搜索引擎切换
 //点击图标显示搜索引擎图标
 document.getElementById('searchtype').addEventListener('click', function () {
@@ -119,32 +80,3 @@ btn.addEventListener('click', () => {
         imgElement.src = "../media/moon.svg";
     }
 });
-
-
-
-//操作json数据实践
-/* var jsonString = '{"name":"John Doe","age":30,"city":"New York"}';
-    var data1 = JSON.parse(jsonString);
-    console.log(data1.name); */
-//可给导航条的项目添加点击事件获取data来实现通过json修改页面，不过不能本地运行
-/* fetch('./json/nav.json')
-    .then(response => response.json()) // 将响应体解析为JSON
-    .then(data => {
-        console.log(data); // 打印整个对象
-        var xx = 'cysite';//可优化为通过点击获取
-        console.log(data[xx]); // 打印对象的属性
-        console.log(data[xx][0]);
-        console.log(data[xx][0].name);
-        var ls = '<a href="' + data[xx][0].link + '" target="_blank">' + data[xx][0].name + '</a>'
-        console.log(ls)
-
-        let result = '';
-        for (const site of data[xx]) {
-            result += '<a href="' + site.link + '" target="_blank">' + site.name + '</a>';
-            //通过获取id插入到指定位置实现修改页面 
-        }
-        console.log(result)
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });  */
